da6d9c4a7c86a0d38fbc8524157519852a8c0b4c
Rafael Steil <rafael@insnecorp.com>
2012-02-25 20:50:11 -0200

    Simplified the layout

--- src/ScreenCapture/AboutForm.Designer.cs
+++ src/ScreenCapture/AboutForm.Designer.cs
@@ -32,12 +32,8 @@
 			this.label2 = new System.Windows.Forms.Label();
 			this.licenseTextBox = new System.Windows.Forms.RichTextBox();
 			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.projectLink = new System.Windows.Forms.LinkLabel();
-			this.label5 = new System.Windows.Forms.Label();
 			this.okButton = new System.Windows.Forms.Button();
 			this.websiteLink = new System.Windows.Forms.LinkLabel();
-			this.label6 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -76,38 +72,9 @@
 			this.label3.TabIndex = 3;
 			this.label3.Text = "License";
 			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(15, 247);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(178, 13);
-			this.label4.TabIndex = 4;
-			this.label4.Text = "This is an Open Source project. Visit";
-			// 
-			// projectLink
-			// 
-			this.projectLink.AutoSize = true;
-			this.projectLink.Location = new System.Drawing.Point(192, 247);
-			this.projectLink.Name = "projectLink";
-			this.projectLink.Size = new System.Drawing.Size(220, 13);
-			this.projectLink.TabIndex = 5;
-			this.projectLink.TabStop = true;
-			this.projectLink.Text = "https://github.com/rafaelsteil/ScreenCapture";
-			this.projectLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.websiteLink_LinkClicked);
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(18, 267);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(138, 13);
-			this.label5.TabIndex = 6;
-			this.label5.Text = "if you want to contribute, or ";
-			// 
 			// okButton
 			// 
-			this.okButton.Location = new System.Drawing.Point(188, 297);
+			this.okButton.Location = new System.Drawing.Point(195, 238);
 			this.okButton.Name = "okButton";
 			this.okButton.Size = new System.Drawing.Size(75, 23);
 			this.okButton.TabIndex = 7;
@@ -118,7 +85,7 @@
 			// websiteLink
 			// 
 			this.websiteLink.AutoSize = true;
-			this.websiteLink.Location = new System.Drawing.Point(154, 267);
+			this.websiteLink.Location = new System.Drawing.Point(18, 244);
 			this.websiteLink.Name = "websiteLink";
 			this.websiteLink.Size = new System.Drawing.Size(105, 13);
 			this.websiteLink.TabIndex = 8;
@@ -126,26 +93,13 @@
 			this.websiteLink.Text = "http://rafaelsteil.com";
 			this.websiteLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.websiteLink_LinkClicked);
 			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(261, 267);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(119, 13);
-			this.label6.TabIndex = 9;
-			this.label6.Text = "for the author\'s website.";
-			// 
 			// AboutForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(448, 332);
-			this.Controls.Add(this.label6);
+			this.ClientSize = new System.Drawing.Size(448, 273);
 			this.Controls.Add(this.websiteLink);
 			this.Controls.Add(this.okButton);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.projectLink);
-			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.licenseTextBox);
 			this.Controls.Add(this.label2);
@@ -167,11 +121,7 @@
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.RichTextBox licenseTextBox;
 		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label label4;
-		private System.Windows.Forms.LinkLabel projectLink;
-		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Button okButton;
 		private System.Windows.Forms.LinkLabel websiteLink;
-		private System.Windows.Forms.Label label6;
 	}
 }
